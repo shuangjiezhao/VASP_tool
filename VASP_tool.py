d8856076190dae3b3ea3fb7b957d2616e127f38b
shuangjiezhao <137879800+shuangjiezhao@users.noreply.github.com>
2024-12-21 19:00:14 +0100

    VASP_tool.py

--- VASP_tool.py
+++ VASP_tool.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter, convolve, gaussian
+from scipy.signal import savgol_filter, convolve
 from scipy.ndimage import gaussian_filter1d
 
 
@@ -31,10 +31,6 @@ class VASP_tool:
             self._read_and_process_data_regularband(files_path, spin_polarization, fermi_level)  # Read and process data
         if function == "regular DOS":
             self._read_and_process_data_regulardos(files_path, spin_polarization, fermi_level)  # Read and process data
-        if function == "Projected band":
-            self._read_and_process_data_projectedband(files_path, spin_polarization, fermi_level)  # Read and process data
-        if function == "Projected DOS":
-            self._read_and_process_data_projecteddos(files_path, spiecies, spin_polarization, fermi_level)  # Read and process data
 
     def _read_and_process_data_regularband(self, files_path, spin_polarization, fermi_level):
         """Reads and processes VASP data files."""
@@ -54,8 +50,8 @@ class VASP_tool:
             raise FileNotFoundError(f"The following files are missing: {', '.join(keys)}")
 
         # Read and process high-symmetry k-points
-        high_symmetry_data = np.genfromtxt(files["KLABELS"], skip_header=1, dtype=str, comments="#")
-        high_symmetry_data[high_symmetry_data[:, 0] == 'GAMMA', 0] = 'G'
+        high_symmetry_data = np.genfromtxt(files["KLABELS"], skip_header=1, dtype=str, comments="*")
+        high_symmetry_data[high_symmetry_data[:, 0] == 'GAMMA', 0] = 'Γ'
         self.high_symmetry = high_symmetry_data
         self.KLABELS = high_symmetry_data[:, 1].astype(float)
 
@@ -300,213 +296,3 @@ class VASP_tool:
             spine.set_linewidth(frame_linewidth)
         else:
             plt.show()
-    
-    def _read_and_process_data_projecteddos(self, files_path, spiecies, spin_polarization, fermi_level):
-
-        # Determine file keys based on spin polarization
-        if spin_polarization:
-            key_up = []
-            key_dw = []
-            for i in spiecies:
-                key_up.append(f'{i}_up')
-                key_dw.append(f'{i}_dw')
-                
-            ##prepare path of files    
-            files_up = {}
-            for entry in os.scandir(files_path):
-                if entry.is_file() and not entry.name.startswith("."): 
-                    for key in key_up:
-                        if key.upper() in entry.name.upper():
-                            files_up[key] = entry.path
-                            key_up.remove(key)  # Remove the key once found to avoid duplicates
-                            break  # Move on to the next file once a match is found
-            if keys:  # Raise an error if any keys were not found in any file
-                raise FileNotFoundError(f"The following files are missing: {', '.join(keys)}") 
-            
-            
-            files_dw = {}
-            for entry in os.scandir(files_path):
-                if entry.is_file() and not entry.name.startswith("."): 
-                    for key in key_dw:
-                        if key.upper() in entry.name.upper():
-                            files_dw[key] = entry.path
-                            key_dw.remove(key)  # Remove the key once found to avoid duplicates
-                            break  # Move on to the next file once a match is found
-            if keys:  # Raise an error if any keys were not found in any file
-                raise FileNotFoundError(f"The following files are missing: {', '.join(keys)}") 
-            
-            self.up_dos = []  # Initialize as a list
-            self.dw_dos = []
-            self.totdos = []
-
-            for file in files_up:
-                self.energy = np.loadtxt(files_up[file], skiprows=1)[:, 0]
-                buffer = np.loadtxt(files_up[file], skiprows=1)[:, 1:]
-                buffer = np.hstack((
-                          buffer[:, 0:1],   # First column
-                          buffer[:, 1:4].sum(axis=1, keepdims=True),  # Sum of 2nd, 3rd, 4th
-                          buffer[:, 4:9].sum(axis=1, keepdims=True)   # Sum of 5th through 9th
-                          buffer[:, 9::].sum(axis=1, keepdims=True) #last column
-                          ))
-                self.up_dos.append(buffer) 
-
-            self.up_dos = np.hstack(self.up_dos)  # Stack into a single NumPy array
-            
-            for file in files_dw:
-                buffer = np.loadtxt(files_dw[file], skiprows=1)[:, 1:]
-                buffer = np.hstack((
-                          buffer[:, 0:1],   # First column
-                          buffer[:, 1:4].sum(axis=1, keepdims=True),  # Sum of 2nd, 3rd, 4th
-                          buffer[:, 4:9].sum(axis=1, keepdims=True)   # Sum of 5th through 9th
-                          buffer[:, 9::].sum(axis=1, keepdims=True) #last column
-                          ))
-                self.dw_dos.append(buffer) 
-
-            self.dw_dos = np.hstack(self.dw_dos)  # Stack into a single NumPy array            
-        else:
-            keys = spiecies
-            files = {}
-            for entry in os.scandir(files_path):
-                if entry.is_file() and not entry.name.startswith("."): 
-                    for key in keys:
-                        if key.upper() in entry.name.upper():
-                            files[key] = entry.path
-                            key_up.remove(key)  # Remove the key once found to avoid duplicates
-                            break  # Move on to the next file once a match is found
-            if keys:  # Raise an error if any keys were not found in any file
-                raise FileNotFoundError(f"The following files are missing: {', '.join(keys)}")
-            
-            for file in files:
-                self.energy = np.loadtxt(files[file], skiprows=1)[:, 0]
-                buffer = np.loadtxt(files[file], skiprows=1)[:, 1:]
-                buffer = np.hstack((
-                          buffer[:, 0:1],   # First column
-                          buffer[:, 1:4].sum(axis=1, keepdims=True),  # Sum of 2nd, 3rd, 4th
-                          buffer[:, 4:9].sum(axis=1, keepdims=True)   # Sum of 5th through 9th
-                          buffer[:, 9::]    #last column
-                          ))
-                self.totdos.append(buffer)
-            self.totdos = np.hstack(self.totdos)
-                
-    def plot_projected_dos(self, save_path=None, spieces = None, orbitals=['tot'], figsize=(6, 12), line_color='k', fermi_color='r',
-                    line_linewidth=2, fermi_linewidth=1.5, x_label='number of states', up_symbol = "\u2191",dw_symbol ="\u2193",
-                    y_label=r'E-E$_{F}$ (eV)', title='PDOS', xlim=None, ylim=None,
-                    dpi=300, label_fontsize=20, legend_size=10, frame_linewidth = 1.5,xtick=20, ytick=2,
-                    ytick_fontsize=16, xtick_fontsize=16, smoothing_method='savgol', window_length=11, polyorder=2,
-                    broadening_method='gaussian', broadening_sigma=0.1,orientation = "vertical", plot_tot = True):
-        
-        fig, ax = plt.subplots(figsize=figsize)
-        spin_polarization = self.spin_polarized
-
-    # Calculate max DOS to determine mirrored x-axis limits
-        max_dos = max(np.abs(self.up_dos).max(), np.abs(self.dw_dos).max())
-        
-        ###orbital contribution indexing
-        orbital_name = {"s":0, "p":1, "d":2, "tot":3}
-        key_list = list(orbital_name.keys())
-        orbital_index = [orbital_name[orbital_key] for orbital_key in orbitals]
-
-        # Smoothing
-        if smoothing_method == 'savgol':
-            up_dos_smoothed = savgol_filter(self.up_dos, window_length, polyorder)
-            dw_dos_smoothed = savgol_filter(self.dw_dos, window_length, polyorder)
-        elif smoothing_method == 'convolution':
-            window = np.ones(window_length) / window_length
-            up_dos_smoothed = np.convolve(self.up_dos, window, mode='same')
-            dw_dos_smoothed = np.convolve(self.dw_dos, window, mode='same')
-
-    # Peak Broadening (applied after smoothing)
-        if broadening_method == 'gaussian':
-            up_dos_broadened = gaussian_filter1d(up_dos_smoothed, sigma=broadening_sigma)
-            dw_dos_broadened = gaussian_filter1d(dw_dos_smoothed, sigma=broadening_sigma)
-    # Add more broadening methods if needed (e.g., Lorentzian)
-        if orientation == "vertical":
-            if spin_polarization:
-                if orbital_index != [3]:
-                    for i,m in zip(orbital_index,spieces):
-                        for j,n in zip(range(i,len(spieces)*4,4),line_color):
-                            up_plot = ax.plot(np.abs(up_dos_broadened[:,j]), self.energy, color=n, 
-                                      linewidth=line_linewidth, label=f'{m}_{key_list[i]}{up_symbol}')[0]
-                            dw_plot = ax.plot(-np.abs(dw_dos_broadened[:,j]), self.energy, color=n, 
-                                      linewidth=line_linewidth, label=f'{m}_{key_list[i]}{dw_symbol}')[0]
-                else:
-                    for i,n in zip(range(3,len(orbital_index)*4,4),line_color):
-                        up_plot = ax.plot(np.abs(up_dos_broadened[:,i]), self.energy, color=n, 
-                                      linewidth=line_linewidth, label=f'{m}{up_symbol}')[0]
-                        dw_plot = ax.plot(-np.abs(dw_dos_broadened[:,i]), self.energy, color=n, 
-                                      linewidth=line_linewidth, label=f'{m}{dw_symbol}')[0]
-            else:
-                if orbital_index != [3]:
-                    
-                        #totdos_smoothed = savgol_filter(self.totdos, window_length, polyorder)
-                        #totdos_broadened = gaussian_filter1d(totdos_smoothed, sigma=broadening_sigma)
-                        #ax.plot(totdos_broadened[:,i], self.energy, color=n, linewidth=line_linewidth)
-
-            self.Fermi = 0
-            ax.axhline(y=self.Fermi, color=fermi_color, linestyle='-.', linewidth=fermi_linewidth)
-            ax.axvline(x=0, color='gray', linestyle='--', linewidth=line_linewidth)
-            if xlim is None:
-                xlim = [-max_dos, max_dos]
-            ax.set_xlim(xlim)
-            if ylim is not None:
-                ax.set_ylim(ylim)
-            ax.set_xlabel(x_label, fontsize=label_fontsize)
-            ax.set_ylabel(y_label, fontsize=label_fontsize)
-            ax.set_title(title, fontsize=label_fontsize)
-            ax.tick_params(axis='both', which='major', labelsize=label_fontsize)
-            ax.legend(fontsize=legend_size,loc='upper right')
-            if xtick is not None:
-                max_dos = max(np.abs(xlim[0]), np.abs(xlim[1]))  # Max absolute value in xlim
-                tick_positions = np.arange(0, max_dos + xtick, xtick)
-                tick_positions = tick_positions.astype(int)
-                tick_labels = [f"{x}" for x in tick_positions]  # Format with one decimal place
-                ax.set_xticks(np.concatenate((-tick_positions[::-1], tick_positions[1:])))  # Mirrored ticks
-                ax.set_xticklabels(tick_labels[::-1] + tick_labels[1:])
-
-            if ytick is not None:
-                ax.set_yticks(np.arange(ylim[0], ylim[1] + ytick, ytick))
-            
-        elif orientation == "horizontal":
-            if spin_polarization:
-                up_plot = ax.plot(self.energy, np.abs(up_dos_broadened), color=line_color[0], 
-                        linewidth=line_linewidth, label="Spin-Up")[0]
-                dw_plot = ax.plot(self.energy, -np.abs(dw_dos_broadened), color=line_color[1], 
-                        linewidth=line_linewidth, label="Spin-Down")[0]
-            else:
-                totdos_smoothed = savgol_filter(self.totdos, window_length, polyorder)
-                totdos_broadened = gaussian_filter1d(totdos_smoothed, sigma=broadening_sigma)
-                ax.plot(self.energy, totdos_broadened, color=line_color, linewidth=line_linewidth)
-            self.Fermi = 0
-            ax.axvline(x=self.Fermi, color=fermi_color, linestyle='-.', linewidth=fermi_linewidth)
-            ax.axhline(y=0, color='gray', linestyle='--', linewidth=line_linewidth)
-            if ylim is None:
-                ylim = [-max_dos, max_dos]
-            ax.set_ylim(ylim)
-            if xlim is not None:
-                ax.set_xlim(xlim)
-            ax.set_xlabel(x_label, fontsize=label_fontsize)
-            ax.set_ylabel(y_label, fontsize=label_fontsize)
-            ax.set_title(title, fontsize=label_fontsize)
-            ax.tick_params(axis='both', which='major', labelsize=label_fontsize)
-            ax.legend(fontsize=legend_size,loc='upper right')
-            if ytick is not None:
-                max_dos = max(np.abs(ylim[0]), np.abs(ylim[1]))  # Max absolute value in xlim
-                tick_positions = np.arange(0, max_dos + ytick, ytick)
-                tick_positions = tick_positions.astype(int)
-                tick_labels = [f"{y}" for y in tick_positions]  # Format with one decimal place
-                ax.set_yticks(np.concatenate((-tick_positions[::-1], tick_positions[1:])))  # Mirrored ticks
-                ax.set_yticklabels(tick_labels[::-1] + tick_labels[1:])
-
-            if xtick is not None:
-                ax.set_xticks(np.arange(xlim[0], xlim[1] + xtick, xtick))
-            
-        else:
-            raise ValueError("Invalid orientation. Choose 'vertical' or 'horizontal'.")
-
-        if save_path is not None:
-            plt.savefig(save_path, dpi=dpi)
-        for spine in ax.spines.values():
-            spine.set_linewidth(frame_linewidth)
-        else:
-            plt.show()        
-  
